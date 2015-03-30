c0768fa1391da1cbaf7196c2787f640e08e32291
Anonoz Chong <honsiongchs@gmail.com>
2015-03-30 12:08:08 +0800

    Fixed heroku URL redir

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   constraints(:host => /makanio.herokuapp.com/) do
-    match "/(*path)" => redirect {|params, req| "https://www.makanio.com/#{params[:path]}"},  via: [:get, :post]
+    match "/(*path)" => redirect {|params, req| "https://www.makan.io/#{params[:path]}"},  via: [:get, :post]
   end
   
   devise_for :vendor_users, class_name: "Vendor::User"
