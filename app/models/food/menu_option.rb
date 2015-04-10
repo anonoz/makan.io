204213aaf291e2a9ebcb365f584449e0acdf3f3c
Anonoz Chong <honsiongchs@gmail.com>
2015-04-10 15:49:27 +0800

    Added specs and validations for food_menu_options

--- app/models/food/menu_option.rb
+++ app/models/food/menu_option.rb
@@ -1,4 +1,7 @@
 class Food::MenuOption < ActiveRecord::Base
   belongs_to :food_menu, class_name: "Food::Menu"
   belongs_to :food_option, class_name: "Food::Option"
+
+  validates :food_menu, presence: true
+  validates :food_option, presence: true
 end
--- spec/models/food/menu_option_spec.rb
+++ spec/models/food/menu_option_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe Food::MenuOption do
-  it "is valid when both food_menu and food_option are associated"
-  it "is invalid without associated food menu"
-  it "is invalid without associated food option"
+  it "is valid when both food_menu and food_option are associated" do
+    expect(build(:food_menu_option)).to be_valid
+  end
+
+  it "is invalid without associated food menu" do
+    menuless = build(:food_menu_option, food_menu: nil)
+    menuless.valid?
+    expect(menuless.errors[:food_menu]).to include "can't be blank"
+  end
+
+  it "is invalid without associated food option" do
+    optionless = build(:food_menu_option, food_option: nil)
+    optionless.valid?
+    expect(optionless.errors[:food_option]).to include "can't be blank"
+  end
 end
