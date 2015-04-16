71a032d1b173f4a027aeb7f928c3330e1ef8d7f5
Anonoz Chong <honsiongchs@gmail.com>
2015-04-16 21:40:12 +0800

    Removed a tab in old migration file and defused a bomb

--- db/migrate/20150331022206_add_default_to_columns_in_food_models.rb
+++ db/migrate/20150331022206_add_default_to_columns_in_food_models.rb
@@ -1,6 +1,6 @@
 class AddDefaultToColumnsInFoodModels < ActiveRecord::Migration
   def change
-  	change_column_default :food_menus, :base_price, 0
+    change_column_default :food_menus, :base_price, 0
     change_column_default :food_options, :jenis, 1
     change_column_default :food_options, :min, 0
     change_column_default :food_options, :max, 0
