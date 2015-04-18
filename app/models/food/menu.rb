ecc1d70c5c1d11352e9d27ee185e75f47b423151
Anonoz Chong <honsiongchs@gmail.com>
2015-04-18 23:58:51 +0800

    Fixed paperclip/s3 for Singapore

--- app/models/food/menu.rb
+++ app/models/food/menu.rb
@@ -21,7 +21,9 @@ class Food::Menu < ActiveRecord::Base
                       medium: "280x210>",
                       thumb: "100x100>"
                     },
-                    default_url: "/images/:style/dddddd.png"
+                    default_url: "/images/:style/dddddd.png",
+                    url: ":s3_domain_url",
+                    preserve_files: true
                     
   validates_attachment_content_type :feature_photo,
                                     content_type: /\Aimage\/.*\Z/
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,7 @@
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:s3_host_name] = ENV["S3_HOST_NAME"]
+
+Paperclip.interpolates(:s3_sg_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-ap-southeast-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},
+  "")}"
+}
