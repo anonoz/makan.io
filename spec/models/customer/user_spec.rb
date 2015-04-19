6ba3312a6705ea11be7d15a5595602fc57864ec5
Anonoz Chong <honsiongchs@gmail.com>
2015-04-19 22:30:50 +0800

    Fixed customer user spec

--- spec/models/customer/user_spec.rb
+++ spec/models/customer/user_spec.rb
@@ -8,16 +8,16 @@ describe Customer::User do
   end
 
   it "is invalid without an encrypted password" do
-  	pwdless_user = build(:customer_user,
-  		                 password: nil,
-  		                 password_confirmation: nil)
-  	pwdless_user.valid?
-  	expect(pwdless_user.errors[:password]).to include "can't be blank"
+    pwdless_user = build(:customer_user,
+                         password: nil,
+                         password_confirmation: nil)
+    pwdless_user.valid?
+    expect(pwdless_user.errors[:password]).to include "can't be blank"
   end
 
   it "is invalid if got same email in database" do
-    create(:customer_user)
-    duplicated_user = build(:customer_user)
+    create(:customer_user, email: "same@same.org")
+    duplicated_user = build(:customer_user, email: "same@same.org")
     duplicated_user.valid?
     expect(duplicated_user.errors[:email]).to include "has already been taken"
   end
