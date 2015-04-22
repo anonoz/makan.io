cd7e828b2d268effd835bacae79569e509292703
Anonoz Chong <honsiongchs@gmail.com>
2015-04-23 00:38:24 +0800

    Fixed food menu creation bug

--- app/models/food/menu_option.rb
+++ app/models/food/menu_option.rb
@@ -4,6 +4,6 @@ class Food::MenuOption < ActiveRecord::Base
   belongs_to :food_menu, class_name: "Food::Menu"
   belongs_to :food_option, class_name: "Food::Option"
 
-  validates :food_menu, presence: true
+  # validates :food_menu, presence: true
   validates :food_option, presence: true
 end
