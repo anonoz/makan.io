27b6b7ee4b507d650accf82d2a96e6591bfb14ba
Anonoz Chong <honsiongchs@gmail.com>
2015-05-06 22:03:57 +0800

    Added spec for orderchit delivery_destination_info

--- spec/models/order/chit_spec.rb
+++ spec/models/order/chit_spec.rb
@@ -31,5 +31,32 @@ describe Order::Chit do
         include "is not included in the list")
     end
   end
+
+  it "returns delivery destination info correctly for offline customer" do
+    order_chit = build(:order_chit,
+                       customer_user: nil,
+                       customer_address: nil,
+                       offline_customer_name: "Ah Beng",
+                       offline_customer_address: "PV10 A-8-8",
+                       offline_customer_phone: "017-3009142")
+
+    info = order_chit.delivery_destination_info
+
+    expect(info[:name]).to eq "Ah Beng"
+    expect(info[:address]).to eq "PV10 A-8-8"
+    expect(info[:phone]).to eq "017-3009142"
+  end
+
+  it "returns delivery destination info correctly for online customer" do
+    user = create(:customer_user)
+    address = create(:customer_address, customer_user: user)
+    order_chit = build(:order_chit, customer_user: user, customer_address: address)
+
+    info = order_chit.delivery_destination_info
+
+    expect(info[:name]).to eq user.name
+    expect(info[:address]).to eq address.human_readable
+    expect(info[:phone]).to eq user.phone
+  end
   
 end
