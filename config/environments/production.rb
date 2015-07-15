0d769cbda7f69e5347cc6ca0b9146da89997859c
Anonoz Chong <honsiongchs@gmail.com>
2015-07-16 01:26:44 +0800

    Test using S3 https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,9 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
+    :s3_protocol => "https",
     :path => ":attachment/:id/:style.:extension",
-    :url => ":s3_domain_url",
+    :url => ":s3_path_url",
     :preserve_files => true
   }
   
